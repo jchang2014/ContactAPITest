9a4612ace1421578dd8c1884ff5c6a1cf3ce40b3
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-12-10 15:06:33 -0800

    Add rescue statements to API calls

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,6 +33,55 @@ users_array = [[1,"n3xeurope@gmail.com"],
 							 [25,"bradleygtucker@gmail.com"]
 							]
 
+	def create_cb_profile
+		@clearbit_response = begin
+			Clearbit::Enrichment.find(email: @user.email, stream: true)
+		rescue Nestful::ResourceInvalid
+			nil
+		end
+		@cb_profile = @user.profiles.create(name: @clearbit_response.try(:person).try(:name).try(:fullName) || "n/a",
+																 title: @clearbit_response.try(:person).try(:employment).try(:title) || "n/a",
+																 company: @clearbit_response.try(:person).try(:employment).try(:name) || "n/a",
+																 photo_url: @clearbit_response.try(:person).try(:avatar) || "n/a",
+																 source: "Clearbit",
+																 tags: "n/a")
+	end
+
+	def create_fc_profile
+		@fullcontact_response = begin
+			FullContact.person(email: @user.email)
+		rescue FullContact::NotFound
+			nil
+		rescue FullContact::Invalid
+			nil
+		end 
+		@fc_profile = @user.profiles.create(name: @fullcontact_response.try(:contact_info).try(:full_name) || "n/a",
+																 				source: "Fullcontact")
+	end
+
+	def find_fc_employment_info
+	  @fc_profile.title = @fullcontact_response.try(:organizations).try(:as,0).try(:title) || "n/a"
+	  @fc_profile.company = @fullcontact_response.try(:organizations).try(:as,0).try(:name) || "n/a"
+	end
+
+	def find_fc_photo_info
+	  @fc_profile.photo_url = @fullcontact_response.try(:photos).try(:as,0).try(:url) || "n/a"
+	end
+
+	def find_fc_tag_info
+		@topics = @fullcontact_response.try(:digital_footprint).try(:topics)
+		if @topics
+			if !@topics.empty?
+				@tags = []
+				@topics.each {|topic| @tags << topic[:value] } 
+				@fc_profile.tags = @tags.join(', ')
+			else
+				@fc_profile.tags = "n/a"
+			end
+		else
+			@fc_profile.tags = "n/a"
+		end
+	end
 
 
 users_array.each do |user|
@@ -43,55 +92,3 @@ users_array.each do |user|
 	find_fc_photo_info
 	find_fc_tag_info
 end
-
-def create_cb_profile
-		@clearbit_response = Clearbit::Enrichment.find(email: @user.email, stream: true)
-		@cb_profile = @user.profiles.create(name: @clearbit_response[:person][:name][:fullName] || "n/a",
-																 title: @clearbit_response[:person][:employment][:title] || "n/a",
-																 company: @clearbit_response[:person][:employment][:name] || "n/a",
-																 photo_url: @clearbit_response[:person][:avatar] || "n/a",
-																 source: "Clearbit",
-																 tags: "n/a")
-	end
-
-	def create_fc_profile
-		@fullcontact_response = FullContact.person(email: @user.email)
-			
-		@fc_profile = @user.profiles.create(name: @fullcontact_response.try(:contact_info).try(:full_name) || "n/a",
-																 				source: "Fullcontact")
-	end
-
-	def find_fc_employment_info
-		if @fullcontact_response[:organizations]
-		  @fc_profile.title = @fullcontact_response.try(:organizations).try(:at,0).try(:title)
-		  @fc_profile.company = @fullcontact_response.try(:organizations).try(:at,0).try(:name)
-		else
-			@fc_profile.title = "n/a"
-			@fc_profile.company = "n/a"
-		end
-	end
-
-	def find_fc_photo_info
-		if @fullcontact_response[:photos]
-		  @fc_profile.photo_url = @fullcontact_response.try(:photos).try(:at,0).try(:url)
-		else
-			@fc_profile.photo_url = "n/a"
-		end
-	end
-
-	def find_fc_tag_info
-		if @fullcontact_response[:digital_footprint]
-			@topics = @fullcontact_response.try(:digital_footprint).try(:topics)
-			if @topics
-				if !@topics.empty?
-					@tags = []
-					@topics.each {|topic| @tags << topic[:value] } 
-					@fc_profile.tags = @tags.join(', ')
-				else
-					@fc_profile.tags = "n/a"
-				end
-			else
-				@fc_profile.tags = "n/a"
-			end
-		end
-	end
