9c49f4d78e12c402e9d067a0b289be1b426fcdf8
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-12-09 22:53:21 -0800

    Remove columns from users table

--- db/migrate/20151210062430_create_users.rb
+++ db/migrate/20151210062430_create_users.rb
@@ -2,12 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
     	t.integer :concierge_id
-    	t.string :name
-    	t.string :email
-    	t.string :title
-    	t.string :company
-    	t.string :photo_url
-    	t.string :source
+      t.string :email
 
       t.timestamps null: false
     end
