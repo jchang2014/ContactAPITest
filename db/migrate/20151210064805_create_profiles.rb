7f9585b90104304d5c727d125327bd5ed3e92f73
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-12-09 22:53:01 -0800

    Create profiles model and migration

---
+++ db/migrate/20151210064805_create_profiles.rb
@@ -0,0 +1,14 @@
+class CreateProfiles < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+    	t.string :name
+    	t.string :title
+    	t.string :company
+    	t.string :photo_url
+    	t.string :source
+    	t.references :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
