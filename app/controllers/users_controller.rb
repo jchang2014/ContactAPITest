5aae143b23c00b9d487bf9fe3239426c289fd246
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-12-11 14:59:26 -0800

    Prepare controller for fc helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,69 +14,7 @@ class UsersController < ApplicationController
 		create_cb_profile(@user) if !@cb_profile
 
 		#@fc_profile = @user.profiles.find_by(source:"Fullcontact")
-
-		#create_fc_profile if !@fc_profile
-	end
-
-
-	private
-	#Need to move these to a lib file
-	
-
-	def create_fc_profile
-		#Query Fullcontact for user data
-		@fullcontact_response = begin
-			FullContact.person(email: @user.email)
-		rescue FullContact::NotFound
-			nil
-		rescue FullContact::Invalid
-			nil
-		end 
-
-		#Save Fullcontact response
-		@user.responses.create(response_hash: @fullcontact_response.to_json, source: "Fullcontact")
-
-		#Create profile from fullcontact response
-		@fc_profile = @user.profiles.create(name: @fullcontact_response.try(:contact_info).try(:full_name) || "n/a",
-																 				source: "Fullcontact")
-
-		#Fill in other attributes
-		find_fc_employment_info
-		find_fc_photo_info
-		find_fc_tag_info
-		find_fc_linkedin_url
-		@fc_profile.save
-	end
-
-	def find_fc_employment_info
-	  @fc_profile.title = @fullcontact_response.try(:organizations).try(:at,0).try(:title) || "n/a"
-	  @fc_profile.company = @fullcontact_response.try(:organizations).try(:at,0).try(:name) || "n/a"
-	end
-
-	def find_fc_photo_info
-	  @fc_profile.photo_url = @fullcontact_response.try(:photos).try(:at,0).try(:url) || nil
-	end
-
-	def find_fc_tag_info
-		@topics = @fullcontact_response.try(:digital_footprint).try(:topics)
-		if @topics
-			if !@topics.empty?
-				@tags = []
-				@topics.each {|topic| @tags << topic[:value] } 
-				@fc_profile.tags = @tags.join(', ')
-			else
-				@fc_profile.tags = "n/a"
-			end
-		else
-			@fc_profile.tags = "n/a"
-		end
-	end
-
-	def find_fc_linkedin_url
-		@social_profiles = @fullcontact_response.try(:social_profiles)
-		@social_profiles.each do |profile|
-			@fc_profile.linkedin_url = profile.try(:url) if profile.try(:type) == "linkedin"
-		end
+		#create_fc_profile(@user) if !@fc_profile
 	end
 
 end
