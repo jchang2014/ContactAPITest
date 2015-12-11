c35abbf6224dc7db618a9d6b7910de3c48e090c9
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-12-11 11:15:52 -0800

    Add linkedin_url column to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211014559) do
+ActiveRecord::Schema.define(version: 20151211191201) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20151211014559) do
     t.string   "source"
     t.string   "tags"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "linkedin_url"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
