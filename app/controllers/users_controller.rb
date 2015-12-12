f173f76c7c4f5dc81e434993e9d552eb923a7a2e
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-12-11 16:32:11 -0800

    Change fullcontacthelpers to class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	extend ClearbitHelpers
+	include ClearbitHelpers
 
 	def index
 		@users = User.all
@@ -10,11 +10,14 @@ class UsersController < ApplicationController
 
 		@user = User.create(email: params[:q]) if !@user
 
-		@cb_profile = @user.profiles.find_by(source:"Clearbit")
-		create_cb_profile(@user) if !@cb_profile
+		#@cb_profile = @user.profiles.find_by(source:"Clearbit")
+		#create_cb_profile(@user) if !@cb_profile
 
-		#@fc_profile = @user.profiles.find_by(source:"Fullcontact")
-		#create_fc_profile(@user) if !@fc_profile
+		@fc_profile = @user.profiles.find_by(source:"Fullcontact")
+		if !@fc_profile
+			@fc_profile = FullContactHelpers.new(@user)
+			@fc_profile.create_fc_profile 
+		end
 	end
 
 end
--- lib/full_contact_helpers.rb
+++ lib/full_contact_helpers.rb
@@ -1,14 +1,15 @@
-module FullContactHelpers
+class FullContactHelpers
 
-	# attr_reader :user
+	attr_reader :user
 
-	# def initialize(user)
-	# 	@user = user
-	# end
-	def create_fc_profile(user)
+	def initialize(user)
+		@user = user
+	end
+
+	def create_fc_profile
 		#Query Fullcontact for user data
 		@fullcontact_response = begin
-			FullContact.person(email: user.email)
+			FullContact.person(email: @user.email)
 		rescue FullContact::NotFound
 			nil
 		rescue FullContact::Invalid
@@ -16,54 +17,53 @@ module FullContactHelpers
 		end 
 
 		#Save Fullcontact response
-		user.responses.create(response_hash: @fullcontact_response.to_json, source: "Fullcontact")
+		@user.responses.create(response_hash: @fullcontact_response.to_json, source: "Fullcontact")
 
 		#Create profile from fullcontact response
-		@fc_profile = user.profiles.create(name: @fullcontact_response.try(:contact_info).try(:full_name) || "n/a",
+		@fc_profile = @user.profiles.create(name: @fullcontact_response.try(:contact_info).try(:full_name) || "n/a",
 																 				source: "Fullcontact")
 
 		#Fill in other attributes
-		find_fc_employment_info(@fullcontact_response, @fc_profile)
-		find_fc_photo_info(@fullcontact_response, @fc_profile)
-		find_fc_tag_info(@fullcontact_response, @fc_profile)
-		find_fc_linkedin_url(@fullcontact_response, @fc_profile)
+		find_fc_employment_info
+		find_fc_photo_info
+		find_fc_tag_info
+		find_fc_linkedin_url
+
+		@fc_profile.save
 	end
 
-	def find_fc_employment_info(response, profile)
-	  profile.title = response.try(:organizations).try(:at,0).try(:title) || "n/a"
-	  profile.company = response.try(:organizations).try(:at,0).try(:name) || "n/a"
-	  profile.save
+	private
+
+	def find_fc_employment_info
+	  @fc_profile.title = @fullcontact_response.try(:organizations).try(:at,0).try(:title) || "n/a"
+	  @fc_profile.company = @fullcontact_response.try(:organizations).try(:at,0).try(:name) || "n/a"
 	end
 
-	def find_fc_photo_info(response, profile)
-	  profile.photo_url = response.try(:photos).try(:at,0).try(:url) || nil
-	  profile.save
+	def find_fc_photo_info
+	  @fc_profile.photo_url = @fullcontact_response.try(:photos).try(:at,0).try(:url) || nil
 	end
 
-	def find_fc_tag_info(response, profile)
-		@topics = response.try(:digital_footprint).try(:topics)
+	def find_fc_tag_info
+		@topics = @fullcontact_response.try(:digital_footprint).try(:topics)
 		if @topics
 			if !@topics.empty?
 				@tags = []
 				@topics.each {|topic| @tags << topic[:value] } 
-				profile.tags = @tags.join(', ')
+				@fc_profile.tags = @tags.join(', ')
 			else
-				profile.tags = "n/a"
+				@fc_profile.tags = "n/a"
 			end
 		else
-			profile.tags = "n/a"
+			@fc_profile.tags = "n/a"
 		end
-		profile.save
 	end
 
-	def find_fc_linkedin_url(response, profile)
-		@social_profiles = response.try(:social_profiles)
+	def find_fc_linkedin_url
+		@social_profiles = @fullcontact_response.try(:social_profiles)
 		@social_profiles.each do |social_profile|
 			if social_profile.try(:type) == "linkedin"
-				profile.linkedin_url = social_profile.try(:url) 
-				profile.save
+				@fc_profile.linkedin_url = social_profile.try(:url) 
 			end
 		end
-		puts "final is:#{profile.linkedin_url}"
 	end
 end
