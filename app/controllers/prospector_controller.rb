8a9c9ec59d9075841b3565d3013e9d750e37b1f3
Jeff Chang <jeffrey.chang2014@gmail.com>
2015-12-15 17:37:22 -0800

    Adjust controller to avoid duplicate users

--- app/controllers/prospector_controller.rb
+++ app/controllers/prospector_controller.rb
@@ -6,14 +6,18 @@ class ProspectorController < ApplicationController
 	def show
 		@people = Clearbit::Prospector.search(domain: params[:company],title: params[:title], email:true)
 
-		ProspectorResponse.create(response_hash: @people.to_json, source:"Prospector")
+		ProspectorResponse.create(response_hash: @people.to_json)
 
 		@people.each do |person|
-			@user = User.create(email:person.email,source:"Prospector")
-			@user.prospector_profiles.create(first_name: person.name.givenName, 
-														last_name: person.name.familyName,
-														full_name: person.name.fullName, 
-														title:person.title)
+			@user = User.where(email: person.email)
+
+			if !@user
+				@user = User.create(email:person.email,source:"Prospector")
+				@user.prospector_profiles.create(first_name: person.name.givenName, 
+																					last_name: person.name.familyName,
+																					full_name: person.name.fullName, 
+																					title:person.title)
+			end
 		end
 	end
 end
